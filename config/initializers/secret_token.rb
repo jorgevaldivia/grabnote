1a29359e2491c778d2ba1728425147fcb18d28ff
Jorge Valdivia <jvaldivia@rarestep.com>
2014-01-05 23:17:15 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Grabnote::Application.config.secret_key_base = '3d4bf8181d14397d1c4f6ef77248b4d332c3939e38bba35556887ff7603bdd4338264747e7a47014ff30d9b83be404b16b25461ae160156c4dd76c323de664af'
