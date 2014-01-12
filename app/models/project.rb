ad3eea8c11d50a4d54a1fcd6fb6770e96abb9dae
Kevin Western <kevinwestern@gmail.com>
2014-01-11 22:28:41 -0800

    Refactored after create to come after relations

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,18 +1,18 @@
 class Project < ActiveRecord::Base
   include HasToken
 
-  after_save :populate_project, on: :create
-
   belongs_to :user
   # TODO(kevin): Extract collaborators
   has_many :project_collaborators
   has_many :collaborators, :through => :project_collaborators, source: :user
   has_many :notes
 
+  after_create :populate_project
+
   protected
     def populate_project
       self.collaborators << self.user
-      self.notes << Project::Note.new(:name => 'Welcome',
+      self.notes << Project::Note.create(:name => 'Welcome',
                                       :project => self)
     end
 end
